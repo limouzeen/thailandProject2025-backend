230be88a8bb436d0f6e07cf9ee2299f804f6191b
limouzeen <amaratkos@hotmail.com>
2025-04-23 06:54:14 +0700

    test

--- server.js
+++ server.js
@@ -1,36 +1,20 @@
-// const express = require('express');
-// const app = express();
-// const authRoutes = require('./routes/authRoutes');
-// const travelRoutes = require('./routes/travelRoutes');
-
-// // ⭐ ใส่ middleware สำหรับ parse body
-
-
-// app.use(express.json()); // แปลง JSON จาก request body
-// app.use(express.urlencoded({ extended: true })); // สำหรับ form-url-encoded (เช่นจาก HTML form)
-
-
-
-
-// app.use('/auth', authRoutes);
-
-// app.use('/travels', travelRoutes);
-
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
 const express = require('express');
-const app = express();
 const authRoutes = require('./routes/authRoutes');
 const travelRoutes = require('./routes/travelRoutes');
 
-// Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-// Routes
+app.use(express.json()); // แปลง JSON จาก request body
+app.use(express.urlencoded({ extended: true })); // สำหรับ form-url-encoded (เช่นจาก HTML form)
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+
 app.use('/auth', authRoutes);
+
 app.use('/travels', travelRoutes);
 
-// Export app for Vercel
-module.exports = app;
+
+app.listen(PORT, () => {
+    console.log("Server is running on port " + PORT + "...");
+});
