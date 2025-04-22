01d669d791c561bb9dcfb6d43eaa1f4a484962c3
limouzeen <amaratkos@hotmail.com>
2025-04-23 06:59:43 +0700

    test

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require("cors");
 const authRoutes = require('./routes/authRoutes');
 const travelRoutes = require('./routes/travelRoutes');
 
@@ -7,13 +8,16 @@ app.use(express.json()); // แปลง JSON จาก request body
 app.use(express.urlencoded({ extended: true })); // สำหรับ form-url-encoded (เช่นจาก HTML form)
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
-
+app.use(cors())
 app.use('/auth', authRoutes);
-
 app.use('/travels', travelRoutes);
 
+
+app.get("/", (req, res) => {
+    res.json({ message: "Hello from Back-end server!.." });
+});
 
 app.listen(PORT, () => {
     console.log("Server is running on port " + PORT + "...");
