1aa8905e229218c828fe5229564194f70a602e6b
limouzeen <amaratkos@hotmail.com>
2025-04-23 11:08:13 +0700

    dd

--- server.js
+++ server.js
@@ -1,28 +1,54 @@
+// const express = require('express');
+// const cors = require("cors");
+// const authRoutes = require('./routes/authRoutes');
+// const travelRoutes = require('./routes/travelRoutes');
+// require("dotenv").config();
+
+// const app = express(); 
+
+// const PORT = process.env.PORT || 3000;
+
+// app.use(cors());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// // Routes
+// app.use('/auth', authRoutes);
+// app.use('/travels', travelRoutes);
+
+// // Default Route
+// app.get("/", (req, res) => {
+//   res.json({ message: "Hello from Back-end server!.." });
+// });
+
+// //เทสโชว์
+// app.listen(PORT, () => {
+//     console.log("Server is running on port " + PORT + "...");
+// });
+  
+
+
 const express = require('express');
-const cors = require("cors");
+const app = express();
+const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 const travelRoutes = require('./routes/travelRoutes');
-require("dotenv").config();
 
-const app = express(); 
-
-const PORT = process.env.PORT || 3000;
+// ⭐ ใส่ middleware สำหรับ parse body
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+  origin: 'http://localhost:4859' // 👈 ตรงกับ frontend
+}));
+app.use(express.json()); // แปลง JSON จาก request body
+app.use(express.urlencoded({ extended: true })); // สำหรับ form-url-encoded (เช่นจาก HTML form)
 
-// Routes
+
+
+
 app.use('/auth', authRoutes);
+
 app.use('/travels', travelRoutes);
 
-// Default Route
-app.get("/", (req, res) => {
-  res.json({ message: "Hello from Back-end server!.." });
-});
-
-//เทสโชว์
-app.listen(PORT, () => {
-    console.log("Server is running on port " + PORT + "...");
-});
-  
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
