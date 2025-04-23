51f971297f3fd54de030b42f9beef6d6e470cf28
limouzeen <amaratkos@hotmail.com>
2025-04-23 08:43:56 +0700

    test

--- cloudinary.js
+++ cloudinary.js
@@ -1,26 +1,25 @@
 require('dotenv').config();
-
 /**
  * Get all travels
  * @param {Request} req - HTTP request object
  * @param {Response} res - HTTP response object
  * @returns {Promise<void>}
  */
-import { v2 as cloudinary } from 'cloudinary';
+import { v2 } from 'cloudinary';
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
 
-cloudinary.config({
+v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const storage = new CloudinaryStorage({
-  cloudinary,
+  cloudinary: v2,
   params: {
     folder: 'travel-images',
     allowed_formats: ['jpg', 'png'],
   },
 });
 
-export default { cloudinary, storage };
+export default { cloudinary: v2, storage };
