94a508c5fe66a51d04ffa2468568e069aac6f60f
limouzeen <amaratkos@hotmail.com>
2025-04-23 07:10:16 +0700

    test

--- server.js
+++ server.js
@@ -21,5 +21,8 @@ app.get("/", (req, res) => {
   res.json({ message: "Hello from Back-end server!.." });
 });
 
-
-module.exports = app;
+//เทสโชว์
+app.listen(PORT, () => {
+    console.log("Server is running on port " + PORT + "...");
+});
+  
