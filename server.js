4b3f31b5dc789cd99ba2461b9c5342840b4c91e7
limouzeen <amaratkos@hotmail.com>
2025-04-24 09:15:49 +0700

    test

--- server.js
+++ server.js
@@ -9,11 +9,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
-const cors = require('cors');
-app.use(cors({
-  origin: 'https://thailand-project2025.vercel.app',
-  credentials: true
-}));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://thailand-project2025.vercel.app");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
